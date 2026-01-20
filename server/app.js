a7d21ef8f752a8dc4f513fb3052e2fe70f9d44a2
Patricia-Sigei <pattychela97@gmail.com>
2026-01-20 15:53:54 +0300

    sets up redis

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 
 // Sample route
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Safaricom Assistant!");
 });
 
 export default app;
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import "dotenv/config";
 import http from "http";
 import app from "./app.js";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 http.createServer(app).listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
