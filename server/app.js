02fb6bfd72406d98faf0c18cd8eade5b13fe444d
Patricia-Sigei <pattychela97@gmail.com>
2026-01-21 11:57:47 +0300

    registers routes and initializes socket.io

--- server/app.js
+++ server/app.js
@@ -2,6 +2,9 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 
+import bundleRoutes from "./src/features/bundles/bundles.routes.js";
+import conversationRoutes from "./src/conversation/conversation.routes.js";
+
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
@@ -9,5 +12,8 @@ app.use(bodyParser.json());
 app.get("/", (req, res) => {
   res.send("Safaricom Assistant!");
 });
+
+app.use("/api/bundles", bundleRoutes);
+app.use("/api/conversation", conversationRoutes);
 
 export default app;
--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
 import "dotenv/config";
 import http from "http";
 import app from "./app.js";
+import { initSocket } from "./src/config/socket.js";
+import e from "express";
 
 const PORT = process.env.PORT || 8080;
 
-http.createServer(app).listen(PORT, () => {
+const server = http.createServer(app);
+const io = initSocket(server);
+
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+export { io };
