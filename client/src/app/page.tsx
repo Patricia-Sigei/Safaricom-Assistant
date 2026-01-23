3f5e9af020fae745ba3fcca12539cc30c291d2ea
Patricia-Sigei <pattychela97@gmail.com>
2026-01-23 13:57:33 +0300

    sets up the home page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,18 @@
 "use client";
 
+import { Box, Typography } from "@mui/material";
 export default function Home() {
   return (
-  
+    <Box
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      height="100vh"
+    >
+      <Typography variant="h3" gutterBottom>
+        Welcome to Safaricom Assistant
+      </Typography>
+    </Box>
   );
 }
